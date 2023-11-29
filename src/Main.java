c42c4c842aab3392034de6c62a557b4fa4c40ad9
Mustafa <Ycesan>
2023-11-29 11:37:01 +0100

    added readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    /**
+     * Takes an array and sorts it in an ascending order. Note that the method is void, so it should
+     * sort the input, rather than return a sorted copy.
+     *
+     * @param arr - the array that needs to be sorted.
+     */
+    public void sort(int[] arr) {
+        // TODO
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MainTest {
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+
+public class MainTest {
+
+    @Test
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    public void example() {
+        int[] input = {4, 2, 5, 1, 3};
+        new Main().sort(input);
+        Assertions.assertArrayEquals(new int[] {1, 2, 3, 4, 5}, input);
+    }
 }
